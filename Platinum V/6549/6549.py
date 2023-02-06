60a34fec5d4c1be129b6f46af5618de65b9921c6
남유찬 <bryan0326@naver.com>
2023-02-07 01:40:49 +0900

    히스토그램에서 가장 큰 직사각형

--- Platinum V/6549/6549.py
+++ Platinum V/6549/6549.py
@@ -1,29 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-val = []
-stk = []
-ans = 0
-for i in range(n):
-    val.append(int(input()))
+while True:
+    stk = []
+    ans = 0
+    val = list(map(int, input().split()))
+    n = val.pop(0)
+    if n == 0:
+        break
 
-for i in range(n):
-    while stk and val[stk[-1]] > val[i]:
+    for i in range(n):
+        while stk and val[stk[-1]] > val[i]:
+            tmp = stk.pop()
+            if stk:
+                width = i - stk[-1] - 1
+            else:
+                width = i
+            ans = max(ans, width*val[tmp])
+        stk.append(i)
+
+    while stk:
         tmp = stk.pop()
         if stk:
-            width = i - stk[-1] - 1
+            width = n-stk[-1]-1
         else:
-            width = i
+            width = n
         ans = max(ans, width*val[tmp])
-    stk.append(i)
 
-while stk:
-    tmp = stk.pop()
-    if stk:
-        width = n-stk[-1]-1
-    else:
-        width = n
-    ans = max(ans, width*val[tmp])
-
-print(ans)
+    print(ans)
