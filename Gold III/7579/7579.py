5656c0ef17d1be28a3b70749bd0deac5770f1827
남유찬 <bryan0326@naver.com>
2024-01-08 13:19:59 +0900

    앱 재채점 오류 수정

--- Gold III/7579/7579.py
+++ Gold III/7579/7579.py
@@ -1,19 +1,16 @@
 n, m = map(int, input().split())
-memory = [0]+list(map(int, input().split()))
-cost = [0]+list(map(int, input().split()))
+memory = list(map(int, input().split()))
+cost = list(map(int, input().split()))
 dp = [[0]*(sum(cost)+1) for _ in range(n+1)]
-ans = 10001
 
 for i in range(1, n+1):
-    byte, c = memory[i], cost[i]
-
-    for j in range(1, sum(cost)+1):
-        if j < c:
-            dp[i][j] = dp[i-1][j]
+    for j in range(0, sum(cost)+1):
+        if j >= cost[i-1]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-cost[i-1]]+memory[i-1])
         else:
-            dp[i][j] = max(byte + dp[i-1][j-c], dp[i-1][j])
+            dp[i][j] = dp[i-1][j]
 
-        if dp[i][j] >= m:
-            ans = min(ans, j)
-
-print(ans if m else 0)
+for i, mem in enumerate(dp[n]):
+    if mem >= m:
+        print(i)
+        break
